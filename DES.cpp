4d3f7c695f022c2fab3622ee78c04467a575d874
lingacharyk <113505257+lingacharyk@users.noreply.github.com>
2023-11-15 13:15:53 +0530

    Update DES.cpp

--- DES.cpp
+++ DES.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Initial permutation table (IP)
+
 const int initialPermutationTable[64] = {
     58, 50, 42, 34, 26, 18, 10, 2,
     60, 52, 44, 36, 28, 20, 12, 4,
@@ -14,21 +14,20 @@ const int initialPermutationTable[64] = {
     63, 55, 47, 39, 31, 23, 15, 7
 };
 
-// Example simplified DES key (56 bits)
+
 const char* exampleKey = "secretk";
 
-// Example message (64 bits)
 const char* exampleMessage = "01234567";
 
-// Simplified DES encryption function
+
 void encryptDES(const char* message, const char* key, char* result) {
-    // Perform initial permutation (IP)
+
     char initialPerm[64];
     for (int i = 0; i < 64; ++i) {
         initialPerm[i] = message[initialPermutationTable[i] - 1];
     }
 
-    // Perform a simple XOR operation as an example
+
     for (int i = 0; i < 64; ++i) {
         result[i] = initialPerm[i] ^ key[i % strlen(key)];
     }
@@ -37,10 +36,9 @@ void encryptDES(const char* message, const char* key, char* result) {
 int main() {
     char encryptedResult[64];
 
-    // Encrypt the example message using the example key
+   
     encryptDES(exampleMessage, exampleKey, encryptedResult);
 
-    // Display the encrypted message (just for demonstration)
     printf("Encrypted message: ");
     for (int i = 0; i < 64; ++i) {
         printf("%d", encryptedResult[i]);
